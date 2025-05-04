84c6d0b2bb40261aae1b23bcc0f907f039900cbd
JoungWoo Lee <j_ra1n@naver.com>
2025-05-04 17:25:14 +0900

    Initial Spring Boot project for Hanwha ICT

---
+++ src/main/java/com/hanwha/ict/contract/contract_system/ContractSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hanwha.ict.contract.contract_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanwha/ict/contract/contract_system/ContractSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanwha.ict.contract.contract_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
